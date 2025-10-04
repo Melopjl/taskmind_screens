94c3b5573e22c6dc2f2fba1531ebd89c60df4464
Melopjl <arturmelo12f@gmail.com>
2025-10-04 19:19:09 -0300

    att apenas telas sem logica,só frontend

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// App.js
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-// Componentes principais
+
 import NavegacaoApp from './src/navegacao/navegacaoapp';
 import Autenticacao from './src/navegacao/autenticacao';
 
 export default function App() {
-  // Estado de login
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
